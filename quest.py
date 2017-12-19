5d9a03425309052a745bba05c679d6628ba181ed
indrajithi <i.indrajith.143@gmail.com>
2017-12-20 00:23:21 +0530

    Added more comments

--- quest.py
+++ quest.py
@@ -1,3 +1,6 @@
+#Author: Indrajith Indraprastham
+#Date:  Wed Dec 20 00:12:39 IST 2017 (last update)
+
 from textblob import TextBlob
 import nltk
 from textblob import Word
@@ -8,8 +11,10 @@ def parse(string):
     """
     Parse a paragraph. Devide it into sentences and try to generate quesstions from each sentences.
     """
+    
     try:
         txt = TextBlob(string)
+        # Each sentence is taken from the string input and passed to genQuestion() to generate questions.
         for sentence in txt.sentences:
             genQuestion(sentence)
 
@@ -22,23 +27,39 @@ def genQuestion(line):
     """
     outputs question from the given text
     """
-    if type(line) is str:
-        line = TextBlob(line)
-
-    bucket = {}
-
-
-    for i,j in enumerate(line.tags):
-        if j[1] not in bucket:
-            bucket[j[1]] = i
     
-    if verbose:
+
+    if type(line) is str:     # If the passed variable is of type string.
+        line = TextBlob(line) # Create object of type textblob.blob.TextBlob
+
+    bucket = {}               # Create an empty dictionary
+
+
+    for i,j in enumerate(line.tags):  # line.tags are the parts-of-speach in English
+        if j[1] not in bucket:
+            bucket[j[1]] = i  # Add all tags to the dictionary or bucket variable
+    
+    if verbose:               # In verbose more print the key,values of dictionary
         print('\n','-'*20)
         print(line ,'\n')        
-        print("TAGS:",line.tags, '\n')
+        print("TAGS:",line.tags, '\n')  
         print(bucket)
     
-    question = ''
+    question = ''            # Create an empty string 
+
+    # These are the english part-of-speach tags used in this demo program.
+    #.....................................................................
+    # NNS     Noun, plural
+    # JJ  Adjective 
+    # NNP     Proper noun, singular 
+    # VBG     Verb, gerund or present participle 
+    # VBN     Verb, past participle 
+    # VBZ     Verb, 3rd person singular present 
+    # VBD     Verb, past tense 
+    # IN      Preposition or subordinating conjunction 
+    # PRP     Personal pronoun 
+    # NN  Noun, singular or mass 
+    #.....................................................................
 
     l1 = ['NNP', 'VBG', 'VBZ', 'IN']
     l2 = ['NNP', 'VBG', 'VBZ']
@@ -52,7 +73,6 @@ def genQuestion(line):
 
     l8 = ['NNP', 'VBZ', 'JJ']
     l9 = ['NNP', 'VBZ', 'NN']
-    #ll9 = ['PRP', 'VBZ', 'NN']
 
     l10 = ['NNP', 'VBZ']
     l11 = ['PRP', 'VBZ']
@@ -60,48 +80,52 @@ def genQuestion(line):
     l13 = ['NN', 'VBZ']
 
 
+    # With the use of conditional statements the dictionary is compared with the list created above
 
     
-
-    if all(key in  bucket for key in l1):
+    if all(key in  bucket for key in l1): #'NNP', 'VBG', 'VBZ', 'IN' in sentence.
         question = 'What' + ' ' + line.words[bucket['VBZ']] +' '+ line.words[bucket['NNP']]+ ' '+ line.words[bucket['VBG']] + '?'
 
-    elif all(key in  bucket for key in l2):
+    
+    elif all(key in  bucket for key in l2): #'NNP', 'VBG', 'VBZ' in sentence.
         question = 'What' + ' ' + line.words[bucket['VBZ']] +' '+ line.words[bucket['NNP']] +' '+ line.words[bucket['VBG']] + '?'
 
-    elif all(key in  bucket for key in l3):
+    
+    elif all(key in  bucket for key in l3): #'PRP', 'VBG', 'VBZ', 'IN' in sentence.
         question = 'What' + ' ' + line.words[bucket['VBZ']] +' '+ line.words[bucket['PRP']]+ ' '+ line.words[bucket['VBG']] + '?'
 
-    elif all(key in  bucket for key in l4):
+    
+    elif all(key in  bucket for key in l4): #'PRP', 'VBG', 'VBZ' in sentence.
         question = 'What ' + line.words[bucket['PRP']] +' '+  ' does ' + line.words[bucket['VBG']]+ ' '+  line.words[bucket['VBG']] + '?'
 
-    elif all(key in  bucket for key in l7):
+    elif all(key in  bucket for key in l7): #'NN', 'VBG', 'VBZ' in sentence.
         question = 'What' + ' ' + line.words[bucket['VBZ']] +' '+ line.words[bucket['NN']] +' '+ line.words[bucket['VBG']] + '?'
 
-    elif all(key in bucket for key in l8):
+    elif all(key in bucket for key in l8): #'NNP', 'VBZ', 'JJ' in sentence.
         question = 'What' + ' ' + line.words[bucket['VBZ']] + ' ' + line.words[bucket['NNP']] + '?'
 
-    elif all(key in bucket for key in l9):
+    elif all(key in bucket for key in l9): #'NNP', 'VBZ', 'NN' in sentence
         question = 'What' + ' ' + line.words[bucket['VBZ']] + ' ' + line.words[bucket['NNP']] + '?'
 
-    elif all(key in bucket for key in l11):
+    elif all(key in bucket for key in l11): #'PRP', 'VBZ' in sentence.
         if line.words[bucket['PRP']] in ['she','he']:
             question = 'What' + ' does ' + line.words[bucket['PRP']].lower() + ' ' + line.words[bucket['VBZ']].singularize() + '?'
 
-    elif all(key in bucket for key in l10):
+    elif all(key in bucket for key in l10): #'NNP', 'VBZ' in sentence.
         question = 'What' + ' does ' + line.words[bucket['NNP']] + ' ' + line.words[bucket['VBZ']].singularize() + '?'
 
-    elif all(key in bucket for key in l13):
+    elif all(key in bucket for key in l13): #'NN', 'VBZ' in sentence.
         question = 'What' + ' ' + line.words[bucket['VBZ']] + ' ' + line.words[bucket['NN']] + '?'
-
 
 
     #elif all(key in bucket for key in l12):
     #    question = 'Who' + line.words[bucket['NNP']] + ' ' + Word(line.words[bucket['NN']]).lemmatize("v") + '?
 
+    # When the tags are generated 's is split to ' and s. To overcome this issue.
     if 'VBZ' in bucket and line.words[bucket['VBZ']] == "’":
         question = question.replace(" ’ ","'s ")
 
+    # Print the genetated questions as output.
     if question != '':
         print('\n', 'Question: ' + question )
    
@@ -110,6 +134,7 @@ def main():
     """
     Accepts a text file as an argument and generates questions from it.
     """
+    #verbose mode is activated when we give -v as argument.
     global verbose 
     verbose = False
 
@@ -119,14 +144,14 @@ def main():
             print('Verbose Mode Activated\n')
             verbose = True
 
-
+    # Open the file given as argument in read-only mode.
     filehandle = open(sys.argv[1], 'r')
     textinput = filehandle.read()
     print('\n-----------INPUT TEXT-------------\n')
     print(textinput,'\n')
     print('\n-----------INPUT END---------------\n')
 
-
+    # Send the content of text file as string to function parse()
     parse(textinput)
 
 if __name__ == "__main__":
